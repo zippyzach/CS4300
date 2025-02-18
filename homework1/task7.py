6a6c1ae0a7d8c2c38a4e8cbd8e7c29af7a5c3f9f
zippyzach <zippyzach29@yahoo.com>
2025-02-18 11:28:31 -0700

    task7 completion
    installed matplotlib and completed task 7

--- homework1/task7.py
+++ homework1/task7.py
@@ -1,2 +1,24 @@
 #task7 - Package Control in DevEdu
 #CS4300 Zachary Donnelly
+
+import matplotlib.pyplot as plt
+
+
+#using matlab plot to test package control
+def matlab():
+    figure, plot = plt.subplots()
+    
+    plot.plot([1, 2, 3, 4])
+    plot.set_ylabel("test y")
+    plot.set_xlabel("test x")
+    plt.show()
+    
+    return plot
+
+#testing the output matlab - verifies that the output is an Axes object as well as verifying the x and y axes labels
+def test_matlab():
+    test_plot = matlab()
+    
+    assert isinstance(test_plot, plt.Axes)
+    assert test_plot.get_ylabel() == "test y"
+    assert test_plot.get_xlabel() == "test x"
