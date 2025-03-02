dbf6552ed3c3bdc9549f7eb5f1d8c253b81d0157
zippyzach <zippyzach29@yahoo.com>
2025-03-02 01:05:44 -0700

    serializers and view sets
    added serializers and view sets

---
+++ homework2/booking/booking/views.py
@@ -0,0 +1,24 @@
+#Zachary Donnelly - CS4300
+
+from rest_framework import viewsets
+from .models import Movie, Seat, Booking
+from .serializers import M_Serializer, S_Serializer, B_Serializer
+
+#movie view set
+class M_View_Set(viewsets.ModelViewSet):
+    
+    queryset = Movie.objects.all()
+    serializer_class = M_Serializer
+
+#seat view set
+class S_View_Set(viewsets.ModelViewSet):
+    
+    queryset = Seat.ojects.all()
+    serializer_class = S_Serializer
+    
+
+#booking view set
+class B_View_Set(viewsets.ModelViewSet):
+    
+    queryset = Booking.objects.all()
+    serializer_class = B_serializer
--- homework2/booking/models.py
+++ homework2/booking/models.py
@@ -1,10 +1,47 @@
-from django.db import models
 
-class Movie(models.Model)
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Movie(models.Model):
+    
+    #movie title
     title = models.CharField(max_length=200)
+    #description of the movie
     description = models.TextField()
+    #release date of the movie
     date = models.DateField()
+    #run time of the movie
     duration = models.DurationField()
     
     def __str__(self):
         return self.title
+        
+
+class Seat(models.Model):
+
+    #using char field to allow for sections and number sequences
+    number = model.CharField(max_length=5)
+    #bool to check if booked or not, not booked(false) by default
+    status = models.booleanField(default = False)
+
+
+    def __str__(self):
+        return self.number
+      
+
+class Booking(models.Model):
+    
+    #the movie title for the booking request
+    #all foreign keys delete any booking if the movie, seat, or user are deleted
+    movie_title = models.ForeignKey(Movie,on_delete=models.CASCADE)
+    #the seat requested in the booking
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    #the user who requested the booking
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #the date the booking was requested
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f" Booking information for movie: {self.movie_title.title} seat #: {self.seat.number} for user: {self.user.username}"
+
