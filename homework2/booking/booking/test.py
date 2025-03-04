d25d72e2ff6089eb911cf3e3f70faffa7b97883b
zippyzach <zippyzach29@yahoo.com>
2025-03-04 00:58:18 -0700

    testng file structure adjustment and movie test
    fixed file structure for tests and added Movie API test

--- homework2/booking/booking/test.py
+++ homework2/booking/booking/test.py
@@ -4,8 +4,11 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Movie, Seat, Booking
 from datetime import date, timedelta
+from rest_framework.test import APIClient
+from rest_framework import status
 
 #tests that movie is created properly and all parts of the model are populated as expected through assertations
+#tests model and API endpoints
 class MovieTest(TestCase):
     
     def test_create(self):
@@ -16,8 +19,27 @@ class MovieTest(TestCase):
         self.assertEqual(movie.description, "a test movie")
         self.assertEqual(movie.date, date(2020,2,2))
         self.assertEqual(movie.duration, timedelta(hours=2,minutes=2))
+
+class MovieAPITest(TestCase):
+        def setUp(self):
+            self.user = User.objects.create_user(username='test user', password='test_password')
+            self.client = APIClient()
+            self.movie = Movie.objects.create(title="Test Movie", description="a test movie",date=date(2020,2,2), duration=timedelta(hours=2,minutes=2))
         
-#tests that seat is created properly and all parts of the model are populated as expected through assertations       
+        def test_create(self):
+            url = '/api/Movies/'
+            data = {
+            'title': 'Test Movie',
+            'description': 'a test movie',
+            'date': '2020-2-2',
+            'duration': '02:02:00',
+            }
+            
+            response = self.client.post(url, data,format='json')
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+ 
+#tests that seat is created properly and all parts of the model are populated as expected through assertations
+#tests model and API endpoints     
 class SeatTest(TestCase):
     
     def test_create(self):
@@ -30,6 +52,7 @@ class SeatTest(TestCase):
         self.assertFalse(seat.booked_status)
     
 #tests that booking is created properly and all parts of the model are populated as expected through assertations
+#tests model and API endpoints
 class BookingTest(TestCase):
     
     def test_create(self):
