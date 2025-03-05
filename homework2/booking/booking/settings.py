4eab9dd502701f54c8f34d3a5689d86178a9ea49
zippyzach <zippyzach29@yahoo.com>
2025-03-04 19:11:43 -0700

    Update settings.py
    attempting to fix rendering problem with api - prepping static files

--- homework2/booking/booking/settings.py
+++ homework2/booking/booking/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
