54fa56e3b5e77d1024832c59d91c47f648657970
zippyzach <zippyzach29@yahoo.com>
2025-03-04 19:49:48 -0700

    static revert
    changing folder name back to staticfiles

--- homework2/booking/booking/settings.py
+++ homework2/booking/booking/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Custom static files directory
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
